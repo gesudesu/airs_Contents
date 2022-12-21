02d288e805aec2d3a725c5ac33a8c0df3e5dd027
syota <117439689+CHOKOOOOO@users.noreply.github.com>
2022-12-21 12:30:31 +0900

    Add files via upload

--- 7_Media/template2.js
+++ 7_Media/template2.js
@@ -4,18 +4,25 @@
 //正誤判定プログラム
 function AnswerCheck(formp){
 
+       
     // 解答の判定
     for(n = 0; n < formp.answer.length; ++n){    // 問題（解答入力欄）繰り返す
     
-        if(formp.answer[n].value == formp.Canswer[n].value){  // 正誤の判定
+        // 正誤の判定　文字変更ver
+        if(formp.answer[n].value.replace(/\s+/g, '') == formp.Canswer[n].value.replace(/\s+/g, '')){  
             
-            formp.Judgement[n].value = "正解";
-            formp.Judgement[n].style.color = "#0000FF"; 
+            formp.Judgement[n].value = "　正解";
         }
         else
-            formp.Judgement[n].value = "不正解";   
-            
+            formp.Judgement[n].value = "　不正解";   
 
+        // 正誤判定　色変更ver
+        if(formp.answer[n].value.replace(/\s+/g, '') == formp.Canswer[n].value.replace(/\s+/g, '')){
+            formp.Judgement[n].style.color = "blue";
+        }
+        else
+            formp.Judgement[n].style.color = "red";
+            
     }
     
 }
@@ -29,9 +36,9 @@ function Answer(formp){
     for(n = 0; n < formp.answer.length; ++n){  
 
        seikai_data = formp.Canswer[n].value ; 
-       formp.answer[n].value = seikai_data;
-       formp.answer[n].style.color = "#FF0000";
-       formp.Judgement[n].value = null;
+       formp.Lanswer[n].value = seikai_data;
+       formp.Lanswer[n].style.color = "#FF0000";
+       
 
 }
 }
