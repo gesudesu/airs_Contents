f5423373f33282675e8b1ee193464f0a37a49ae1
gesudesu <117792221+gesudesu@users.noreply.github.com>
2023-01-04 15:33:54 +0900

    Add files via upload

---
+++ 12_2_JSON/template1.js
@@ -0,0 +1,32 @@
+function AnswerCheck(formp){
+    
+    let obj = document.getElementById("sample");
+
+    
+
+        if(formp.answer.value == formp.Canswer.value){  // 正誤の判定
+
+            obj.textContent = "　正解";
+            obj.style.color =  "#0000FF";  
+        
+        }
+        else
+            obj.textContent = "　不正解";
+            
+    }
+
+//正解の表示のプログラム
+function Answer(formp){
+
+    let obj = document.getElementById("sample");
+
+    seikai_data = "";    // 正解データ作成用
+    
+    
+    //　正解の表示
+    seikai_data = formp.Canswer.value ; 
+    formp.answer.value = seikai_data;
+    formp.answer.style.color = "#FF0000";
+    obj.textContent = null;
+   
+}
