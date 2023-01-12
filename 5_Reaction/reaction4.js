7487ef5582d0b5e3611a58e83c99c3f28c8bbda0
syota <117439689+CHOKOOOOO@users.noreply.github.com>
2023-01-13 02:57:12 +0900

    Add files via upload

---
+++ 5_Reaction/reaction4.js
@@ -0,0 +1,58 @@
+//　answer　=　入力した解答
+//　Canswer　=　正解の解答
+
+//正誤判定プログラム
+function AnswerCheck(formp){
+
+       
+    // 解答の判定
+    for(n = 0; n < formp.answer.length; ++n){    // 問題（解答入力欄）繰り返す
+    
+        // 正誤の判定　文字変更ver
+        if(formp.answer[n].value.replace(/\s+/g, '') == formp.Canswer[n].value.replace(/\s+/g, '') || formp.answer[n].value.replace(/\s+/g, '') == "inti=1;i<=19;i++" || formp.answer[n].value.replace(/\s+/g, '')=="i>=11"){  
+            
+            formp.Judgement[n].value = "　正解";
+        }
+        else
+            formp.Judgement[n].value = "　不正解";   
+
+        // 正誤判定　色変更ver
+        if(formp.answer[n].value.replace(/\s+/g, '') == formp.Canswer[n].value.replace(/\s+/g, '')|| formp.answer[n].value.replace(/\s+/g, '') == "inti=1;i<=19;i++" || formp.answer[n].value.replace(/\s+/g, '')=="i>=11"){
+            formp.Judgement[n].style.color = "blue";
+        }
+        else
+            formp.Judgement[n].style.color = "red";
+    }
+    
+}
+
+//正解の表示のプログラム
+function Answer(formp){
+    
+    seikai_data = "";    // 正解データ作成用
+
+    //　正解の表示
+    for(n = 0; n < formp.answer.length; ++n){  
+
+       seikai_data = formp.Canswer[n].value ; 
+       formp.Lanswer[n].value = seikai_data;
+       formp.Lanswer[n].style.color = "#FF0000";
+       formp.Lanswer[0].value = "int i = 1; i < 20; i++　or　int i =1; i <= 19; i++";
+       formp.Lanswer[1].value = "i > 10　or　i >=11";
+       
+
+}
+}
+
+//Enterキーでの処理
+currentFNo = 0;
+function nextForm()
+{
+         if(event.keyCode == 13)
+         {
+            currentFNo++;
+            currentFNo %= document.answerForm.elements.length;
+            document.answerForm[currentFNo].focus();
+         }
+}
+window.document.onkeydown = nextForm
