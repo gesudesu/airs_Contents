9f3bef10260638992f48b6dfafa25719df6fda57
syota <117439689+CHOKOOOOO@users.noreply.github.com>
2023-01-13 10:34:45 +0900

    Add files via upload

---
+++ 6_Translate/translate5.js
@@ -0,0 +1,49 @@
+function AnswerCheck(formp){
+    
+    let obj = document.getElementById("sample");
+
+       //文字列の空白の削除
+       var Answer = formp.answer.value.replace(/\s+/g, '');
+       var Canswer = formp.Canswer.value.replace(/\s+/g, '');
+
+       //正誤の判定　文字ver
+        if(Answer == Canswer || Answer == "scale(1/2);"){ 
+            obj.textContent = "　正解";
+        }
+        else
+            obj.textContent = "　不正解";        
+    
+        //　正誤の判定　色を変更ver
+        if(Answer == Canswer|| Answer == "scale(1/2);"){  
+            obj.style.color = "blue";
+        }
+        else
+            obj.style.color = "red";      
+}
+
+//正解の表示のプログラム
+function Answer(formp){
+
+    let obj = document.getElementById("sample");
+
+    seikai_data = "";    // 正解データ作成用
+    
+    //　正解の表示
+    seikai_data = formp.Canswer.value; // 正解の文字列
+    formp.Lanswer.value = seikai_data + "　or　scale(1/2);";
+    formp.Lanswer.style.color = "#FF0000";
+   
+}
+
+//Enterキーでの処理
+currentFNo = 0;
+function nextForm()
+{
+         if(event.keyCode == 13)
+         {
+            currentFNo++;
+            currentFNo %= document.answerForm.elements.length;
+            document.answerForm[currentFNo].focus();
+         }
+}
+window.document.onkeydown = nextForm
